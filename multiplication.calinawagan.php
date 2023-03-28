e1f9a36c01cfdc7250d2ae951b4ee4cf7b2d8025
jaykyrone <93993448+jaykyrone@users.noreply.github.com>
2023-03-28 16:26:55 +0800

    Add files via upload

---
+++ multiplication.calinawagan.php
@@ -0,0 +1,18 @@
+<hmtl>
+<head><title>MULTIPLICATION</title>
+</head>
+<body>
+    <center>
+    <h2>MULTIPLICATION TABLE OF THE NUMBER 10!</h2>
+    <?php
+        $number = 10;
+
+    for($i=1; $i<=10; $i++){
+            echo $number ."x". $i ."=". $number * $i ."<br>";
+        // echo "Kyrone";
+        }
+    ?>
+    </center>
+</body>
+</html>
+Write to Kyrone Jay Calinawagan
